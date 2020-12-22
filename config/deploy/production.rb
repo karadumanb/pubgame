cbc81b6678169c0dc2bdf06851e1005a194c2a37
karadumanb <karadumanbaturay@gmail.com>
2020-12-23 01:05:25 +0300

    keys

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -42,7 +42,7 @@ server '138.68.97.43', user: 'deploy', roles: %w{app db web}
 # Global options
 # --------------
  set :ssh_options, {
-   keys: %w(/home/baturaykaraduman/.ssh/id_rsa),
+   keys: %w(/Users/baturaykaraduman/.ssh/.ssh/id_rsa),
    port: 3000
  }
 #
